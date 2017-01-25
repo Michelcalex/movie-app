9cedefae317c7af2445f2587447f48cdde963131
Michelcalex <Michelcalex+github@gmail.com>
2017-01-25 14:26:14 -0500

    Created MovieService and added js to AddMovieController

--- js/app.js
+++ js/app.js
@@ -1,2 +1,37 @@
 const app = angular.module('MovieApp', []);
 
+
+app.controller('AddMovieController', function ($scope, MovieService) {
+    console.log('Add Movie Controller here');
+    $scope.name = '';
+    $scope.releaseDate = '';
+    $scope.genre = '';
+
+    $scope.movies = MovieService.getMovies();
+
+    $scope.addMovie = function () {
+        MovieService.addMovie($scope.name, $scope.releaseDate, $scope.genre)
+        $scope.name = '';
+        $scope.releaseDate = '';
+        $scope.genre = '';
+    }
+
+    console.log(MovieService.getMovies());
+});
+
+
+
+
+app.factory('MovieService', function() {
+    let movies = [];
+
+    return {
+        addMovie: function(name, date, genre) {
+            movies.push(name, date, genre);
+        },
+
+        getMovies: function() {
+            return movies;
+        },
+    };
+});
